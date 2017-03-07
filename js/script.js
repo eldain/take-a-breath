84e84d3d6667ac7fdc240b7ac36a3f0f7dcbcf58
Daniel McInnis <dmcinnis@uci.edu>
2017-03-07 12:01:13 -0800

    Another slight code clean up

--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
+// Variables
 const patternButtons = document.querySelectorAll("button");
+// End Variables
 
+
+// Functions
+// End Functions
+
+
+// Event Listeneres and Function Calls
 patternButtons.forEach(button => button.addEventListener("click", () => {
 	patternButtons.forEach(button => button.classList.remove("bg-light-blue"));
 	button.classList.add("bg-light-blue");
 }));
+// End Event Listeners and Functions Calls
